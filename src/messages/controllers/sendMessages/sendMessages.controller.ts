42e09d40043f8608fec29bb3d08ca5252c62285a
Caio Eduardo Ragazzi Gemignani <ca.ragazzi@gmail.com>
2020-08-06 18:02:48 -0300

    ajustes

--- src/messages/controllers/sendMessages/sendMessages.controller.ts
+++ src/messages/controllers/sendMessages/sendMessages.controller.ts
@@ -9,7 +9,8 @@ export class SendMessagesController {
   @Post('messages/send')
   sendMessages(@Body(ValidationPipe) sendMessagesDto: SendMessagesDTO): string {
     let columns: string[] = [];
-    sendMessagesDto.columnSheet[Object.keys(sendMessagesDto.columnSheet)[0]].map((itemColumnSheed, idx) => {
+    const responseArray = [];
+    sendMessagesDto.columnSheet[Object.keys(sendMessagesDto.columnSheet)[0]].map(async (itemColumnSheed, idx) => {
       if (idx === 0) {
         columns = itemColumnSheed;
         return
@@ -21,8 +22,15 @@ export class SendMessagesController {
           formatedMessage = formatedMessage.replace(`{${itemColumns}}`, itemColumnSheed[idx])
         }
       })
-      this.sendMessagesService.execute(itemColumnSheed[0], formatedMessage)
+      await this.sendMessagesService.execute(itemColumnSheed[0], formatedMessage).then(response => {
+        responseArray.push({ phone: itemColumnSheed[0], status: response })
+      }).catch(error => {
+        responseArray.push({ phone: itemColumnSheed[0], status: error })
+      })
     })
+
+    console.log(responseArray);
+
     return "ok";
   }
 }
--- src/messages/controllers/sendMessages/sendMessages.service.ts
+++ src/messages/controllers/sendMessages/sendMessages.service.ts
@@ -10,7 +10,25 @@ export class SendMessagesService {
         'debuggerAddress': '127.0.0.1:9222'
     };
 
-    jsnum(phoneNumber: string): string {
+    async execute(phoneNumber: string, message: string): Promise<any> {
+
+        const builder = new Builder().forBrowser(this.browserName).usingServer('http://localhost:4444/wd/hub');
+        const browserCapabilities = Capabilities.chrome().set(this.capabilityName, this.browserOptions);
+        const driver = builder.withCapabilities(browserCapabilities).build();
+        const formattedMessage = this.formatMessage(message);
+
+        try {
+            await driver.executeScript(this.jsnum(phoneNumber))
+            await driver.executeScript(this.jstext(formattedMessage))
+            return "OK"
+        } catch (error) {
+            return "ERROR"
+        } finally {
+            driver.quit();
+        }
+    }
+
+    private jsnum(phoneNumber: string): string {
         return "const openChat = phone => { " +
             "const link = document.createElement('a'); " +
             "link.setAttribute('href', 'whatsapp://send?phone=55" + phoneNumber + "');" +
@@ -20,7 +38,7 @@ export class SendMessagesService {
             "openChat();";
     }
 
-    jstext(message: string): string {
+    private jstext(message: string): string {
         return "var eventFire = (MyElement, ElementType) => " +
             "{ var MyEvent = document.createEvent(\"MouseEvents\"); " +
             "MyEvent.initMouseEvent (ElementType, true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null); " +
@@ -37,23 +55,7 @@ export class SendMessagesService {
             "myFunc();";
     }
 
-    execute(phoneNumber: string, message: string): void {
-
-        let builder = new Builder().forBrowser(this.browserName).usingServer('http://localhost:4444/wd/hub');
-        let browserCapabilities = Capabilities.chrome().set(this.capabilityName, this.browserOptions);
-        let driver = builder.withCapabilities(browserCapabilities).build();
-
-        const formattedMessage = this.formatMessage(message);
-
-        console.log(formattedMessage);
-
-        driver.executeScript(this.jsnum(phoneNumber)).then(async () => {
-            await driver.executeScript(this.jstext(formattedMessage))
-            driver.quit();
-        });
-    }
-
-    formatMessage(message: string){
+    formatMessage(message: string): string {
         return message.replace(/\n/g, "</br>");
     }
 
