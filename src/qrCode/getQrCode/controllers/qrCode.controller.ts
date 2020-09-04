f8acbc7d762a3a3fc2e4d6808a2bdcfe6eaebaac
sahev <s4mus.sh@gmail.com>
2020-09-04 15:10:45 -0300

    alteração de propriedades do getqrcode

--- src/qrCode/getQrCode/controllers/qrCode.controller.ts
+++ src/qrCode/getQrCode/controllers/qrCode.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { QRCodeService } from '../services/qrCode.service';
+import { JwtAuthGuard } from 'src/auth/passport/guards/jwt-auth.guard';
+
 
 @Controller('getqrcode')
 export class QRCodeController {
   constructor(private QRCodeService: QRCodeService) { }
+  @UseGuards(JwtAuthGuard)
   @Get()
   async findAll(): Promise<any> {
     let string = await this.QRCodeService.getqrcode();
